711bab2d344c451a23cfcdaafbc186cc03cff5b7
Joshua Medlen <joshua246@gmail.com>
2020-08-10 20:28:47 -0500

    goats are properly in the corral and print!

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.scss';
+import goatData from '../helpers/data/goatData';
+import GoatCorral from '../components/GoatCorral/GoatCorral';
 
 class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    this.setState({ goats });
+  }
+
   render() {
+    const { goats } = this.state;
+
     return (
       <div className="App">
         <h2>React Goats</h2>
-        <button className="btn btn-info">I am a button</button>
+        <GoatCorral goats={goats}/>
       </div>
     );
   }
